24ba7dbef13f52359ed8b624bf1178d4813d285f
Saptarshi Dutta <saptarshidutta2001@gmail.com>
2024-06-23 23:50:23 +0530

    Adding the rest

---
+++ httpproto.py
@@ -0,0 +1,172 @@
+import socket
+import threading
+import os
+import signal
+import mimetypes
+import logging
+import json
+import cgi
+from datetime import date
+class TcpServer:
+	host='127.0.0.1'
+	port=8080
+	conn=0
+	addr=0
+	allconn=[]
+	def start(self):
+		sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+		sock.bind((self.host,self.port))
+		print("listening at ",sock.getsockname())
+		sock.listen(1)
+		try:
+			while True:
+				self.conn,self.addr=sock.accept()
+				t=threading.Thread(target=self.handle_client,args=(self.conn,self.addr))
+				t.start()
+				self.allconn.append(t)	
+		except socket.error:
+			print("coudnt connect with the socket")
+			sys.exit(1)
+		
+		finally:
+			if sock:
+				sock.close()
+			for t in self.allconn:
+				t.join()
+	def handle_client(self,conn,addr):
+		data=conn.recv(1024)
+		response=self.handle_request(data)
+		conn.sendall(response)
+		conn.close()
+		
+	def handle_request(self,data):
+		return data
+	
+class HttpServer(TcpServer):
+	
+	cntlngth=''
+	headers={
+		'Content-Type':'text/html ',
+		'Server':'First server ',
+		'Date'  :str(date.today()),
+		'Content-length':''
+		}
+	status_codes={
+			200:'Ok \n',
+			404:'Not found \n',
+			501:'Not implemented \n'
+		}
+	def __init__(self):
+		self.uri=None
+		self.method=None
+		self.logger=logging.getLogger()
+		self.logger.setLevel(logging.INFO)
+		self.evnt_logger=logging.FileHandler("access.log",mode='w')
+		self.evnt_logger.setFormatter(logging.Formatter('%(asctime)s  %(message)s'))
+		
+		
+	def handle_request(self,data):
+		blank_line = b'\r\n'
+		res = self.parse(data)
+		cntlngth=len(res)
+		dict2={'Content-length':cntlngth}
+		self.headers.update(dict2)
+		if  res==b'':
+			head=self.get_headers(self.headers).encode()
+			stscds=self.get_codes(404)
+			body=b'<h1>Error 404 Page not found</h1>'
+			return b"".join([stscds,head,blank_line,body])
+
+		elif res==b'Not operatable':
+			head=self.get_headers(self.headers).encode()
+			stscds=self.get_codes(501)
+			body=b'<h1>Error 501 Not Implemented</h1>'
+			return b"".join([stscds,head,blank_line,body])
+
+		elif res==b'Deleted':
+			head=self.get_headers(self.headers).encode()
+			stscds=self.get_codes(200)
+			body=b'<h1>File Deleted</h1>'
+			return b"".join([stscds,head,blank_line,body])
+
+        
+		elif res==b'return head':
+			head=self.get_headers(self.headers)
+			stscds=self.get_codes(200)
+			return b"".join([stscds,head.encode(),blank_line])
+		
+		elif res==b'Form post done':
+			dict3={'Content-Type':'multipart/form-data'}
+			self.headers.update(dict3)
+			head=self.get_headers(self.headers).encode()
+			stscds=self.get_codes(200)
+			body=b'<h1>Happy hacking</h1>'
+			return b"".join([stscds,head,blank_line,body])
+
+            
+		else:
+			self.logger.addHandler(self.evnt_logger)
+			self.logger.info("Connected to"+" "+f"{self.addr}"+"and page "+" "+f"{self.uri}"+"requested")
+			head=self.get_headers(self.headers)
+			stscds=self.get_codes(200)
+			return b"".join([stscds,head.encode(),blank_line,res])
+	
+		
+	def parse(self,data):
+		lines=data.split(b"\r\n")
+		reqline=lines[0]
+		self.method=reqline.decode().split(' ')[0]
+		if self.method!='GET' and self.method!='DELETE' and self.method!='POST'  and self.method!='HEAD':
+			return b'Not operatable'
+		elif self.method=='DELETE':
+			self.handle_DELETE(reqline)
+			return b'Deleted'
+		elif self.method=='HEAD':
+			return b'return head'
+		elif self.method=='POST':
+			self.handle_POST(reqline)
+			return b'Form post done'
+	
+		response=self.handle_GET(reqline)
+		return response
+		
+	    
+    
+
+	def handle_DELETE(self,reqline):
+		body=b''
+		start=reqline.decode().find('/')+1
+		end=reqline.decode().find(' ',start)
+		self.uri=reqline[start:end]
+		if os.path.exists(self.uri):
+			os.remove(self.uri)
+							
+	def handle_GET(self,reqline):
+		body=b''
+		start=reqline.decode().find('/')+1
+		end=reqline.decode().find(' ',start)
+		self.uri=reqline[start:end].decode()
+		if os.path.exists(self.uri) and not os.path.isdir(self.uri):
+			with open(self.uri,'rb')as r:    	
+				body=r.read()
+		return body
+
+	def get_headers(self,headers):
+		content_type = mimetypes.guess_type(self.uri)[0] or 'application/octet-stream'
+		extra_header={'Content-Type':content_type}
+		self.headers.update(extra_header)
+        
+		header_lines = [f'{key}: {value}' for key, value in headers.items()]
+		
+		return '\r\n'.join(header_lines)+'\r\n'
+	
+	
+	def get_codes(self,code):
+		msg=self.status_codes[code]
+		msgresponse='HTTP/1.1 %s %s \r \n'%(code,msg)
+		return msgresponse.encode()
+		
+if __name__ == '__main__':
+	server=HttpServer()
+	server.start()
+
